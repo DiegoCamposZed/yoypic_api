0eec577bae042dd072758b7b5e5efb2916dfdf46
Enrique <enrique.enguix@bitmonlab.com>
2016-09-01 10:33:20 +0200

    * Fixed JSON body in notifications to firebase

--- app/controllers/Notifications.php
+++ app/controllers/Notifications.php
@@ -55,7 +55,7 @@ class Notifications
                                 'Content-Type'  => 'application/json',
                                 'Authorization' => 'key=AIzaSyDQOmiaVwz_F13I_eA7pODoguCHOjrElKM',
                             ],
-                            $responseBody,
+                            json_encode($responseBody),
                             []
                         );
 
